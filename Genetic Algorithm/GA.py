65b62d140d3fb56e14afaef2feb892e21817d7bf
Sergiy Nikon <sergiy.nikon2014@gmail.com>
2018-12-17 17:38:11 +0200

    cosmetic corrections
    deleted all commented code and the large lines splitted into multiple lines

--- Genetic Algorithm/GA.py
+++ Genetic Algorithm/GA.py
@@ -8,12 +8,11 @@ def fitness(f, population):
 def select_parents(population, fitness, num_parents=2):
     # roulette wheel choice
     parents = np.empty((num_parents, population.shape[1]))
-    # normalized_fitness = \
-    #     fitness / np.linalg.norm(fitness)  # values from 0 to 1
 
-    # my way to set up fitnesses with positive numbers
-    normalized_fitness = \
-        1 - (fitness - np.min(fitness)) / (np.max(fitness) - np.min(fitness))
+    def normalize_fitness():  # set up fitnesses with positive numbers
+        return 1 - (fitness - np.min(fitness)) / (np.max(fitness) - np.min(fitness))
+
+    normalized_fitness = normalize_fitness()
     selection_probs = normalized_fitness / np.sum(normalized_fitness)
 
     for i in range(num_parents):
@@ -29,14 +28,6 @@ def select_parents(population, fitness, num_parents=2):
 
 
 def crossover(parents, offspring_size):
-    # offspring = np.empty(offspring_size)
-    # crossover_point = np.uint8(offspring_size[1] / 2)
-    # for k in range(offspring_size[0]):
-    #     offspring[k, 0:crossover_point] = \
-    #         parents[0, 0:crossover_point]
-    #     offspring[k, crossover_point:] = \
-    #         parents[1, crossover_point:]
-
     # whole ariphmetic crossover
     alpha = 0.6
     offspring = np.empty(offspring_size)
@@ -46,7 +37,7 @@ def crossover(parents, offspring_size):
 
 
 def mutation(offspring_crossover, left, right):
-        # mutation change only 1 gene in each offspring randomly
+    # mutation change only 1 gene in each offspring randomly
     number_of_mutated_genes = 2
     gene_index = np.random.choice(offspring_crossover.shape[1],
                                   size=number_of_mutated_genes,
--- Genetic Algorithm/minimum_function.py
+++ Genetic Algorithm/minimum_function.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 
 class Genetics(object):
 
-    def __init__(self, f, left, right, pop_weight=10, chromosome_length=8, num_generations=100):
-        self.f = f  # function to find the minimum
+    def __init__(self, f, left, right,
+                 pop_weight=10, chromosome_length=8, num_generations=100):
+        self.f = f  # function to find the minimum of it
         self.left = left  # left border of function
         self.right = right  # right border of function
         self.pop_size = (pop_weight, chromosome_length)
@@ -41,8 +42,7 @@ class Genetics(object):
                                              self.left,
                                              self.right)
 
-        # creating new population
-
+        # creating a new population
         self.population[0:parents.shape[0], :] = parents
         self.population[parents.shape[0]:, :] = offspring_mutation
 
@@ -53,34 +53,29 @@ class Genetics(object):
         print("population:\n{0}".format(self.population))
         print("minimum fitness: {0}".format(np.min(self.fitness)))
 
-        # print("best solution: {0}".format(self.population[min_fitness_idx, :]))
-        # print("best solition fitness: {0}".format(
-        #     self.fitness[min_fitness_idx]))
-
     def plot(self):
         x = np.arange(self.left, self.right, 0.01)
         y = self.f(x)
         plt.plot(x, y)
-        # plt.plot(self.f(np.min(self.fitness)), 'o')
 
     def histogram_fitness(self):
         plt.plot(np.arange(self.num_generations + 1), self.fitness_history)
         plt.plot(np.arange(self.num_generations + 1),
-                 np.ones(self.num_generations + 1) * self.minimum_function, 'r-')
+                 np.ones(self.num_generations + 1) * self.minimum_function,
+                 'r-')
         plt.annotate('minimum function: {}'.format(self.minimum_function),
-                     xy=(self.num_generations + 1, self.minimum_function), xytext=(self.num_generations / 3, self.minimum_function + 1),
+                     xy=(self.num_generations + 1, self.minimum_function),
+                     xytext=(self.num_generations / 3,
+                             self.minimum_function + 1),
                      arrowprops=dict(arrowstyle="->"))
 
     def loop(self):
         for i in range(self.num_generations):
-            # self.show()
-
             self.next_generation()
 
 
 def main():
-    ga = Genetics(lambda x: np.exp(-x) * np.cos(2 * np.pi * x),
-                  -4, 4)
+    ga = Genetics(lambda x: np.exp(-x) * np.cos(2 * np.pi * x), -4, 4)
     ga.loop()
     ga.plot()
     plt.show()
